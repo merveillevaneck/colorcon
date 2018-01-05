dd7f1d9bb463a9074925f519a8e1b2a2d5286302
merveillevaneck <merveillevaneck@gmail.com>
2018-01-05 12:16:10 +0200

    updated switch statement for underlined color codes

--- Console.java
+++ Console.java
@@ -14,17 +14,17 @@ public class Console {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
-	public static final String BLACK_BACKGROUND = "\033[40m";
+	public static final String ANSI_BLACK_BACKGROUND = "\033[40m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-	public static final String BLACK_UNDERLINED = "\033[4;30m";  
-    public static final String RED_UNDERLINED = "\033[4;31m";   
-    public static final String GREEN_UNDERLINED = "\033[4;32m";
-    public static final String YELLOW_UNDERLINED = "\033[4;33m";
-    public static final String BLUE_UNDERLINED = "\033[4;34m"; 
-    public static final String PURPLE_UNDERLINED = "\033[4;35m";
-    public static final String CYAN_UNDERLINED = "\033[4;36m"; 
-    public static final String WHITE_UNDERLINED = "\033[4;37m";
+	public static final String ANSI_BLACK_UNDERLINED = "\033[4;30m";  
+    public static final String ANSI_RED_UNDERLINED = "\033[4;31m";   
+    public static final String ANSI_GREEN_UNDERLINED = "\033[4;32m";
+    public static final String ANSI_YELLOW_UNDERLINED = "\033[4;33m";
+    public static final String ANSI_BLUE_UNDERLINED = "\033[4;34m"; 
+    public static final String ANSI_PURPLE_UNDERLINED = "\033[4;35m";
+    public static final String ANSI_CYAN_UNDERLINED = "\033[4;36m"; 
+    public static final String ANSI_WHITE_UNDERLINED = "\033[4;37m";
 
 
     public static String level = "INFO";
@@ -51,8 +51,28 @@ public class Console {
                 return ANSI_RESET;
             case "b":
                 return ANSI_BOLD;
-            case "black":
-                return ANSI_BLACK;
+			case "bb": 
+				return ANSI_BLACK_BACKGROUND;
+			case "wb":
+				return ANSI_WHITE_BACKGROUND;
+			case "rb":
+				return ANSI_RED;
+			case "bu":
+				return ANSI_BLACK_UNDERLINED;
+			case "wu":
+				return ANSI_WHITE_UNDERLINED;
+			case "ru":
+				return ANSI_RED_BACKGROUND;
+			case "gu":
+				return ANSI_GREEN_UNDERLINED;
+			case "yu":
+				return ANSI_CYAN_UNDERLINED;
+			case "blueu":
+				return ANSI_BLUE_UNDERLINED;
+			case "pu":
+				return ANSI_PURPLE_UNDERLINED;
+			case "cu":
+				return ANSI_CYAN_UNDERLINED;
             case "red":
             case "r":
                 return ANSI_RED;
@@ -73,10 +93,8 @@ public class Console {
             case "white":
             case "w":
                 return ANSI_WHITE;
-			case "wb":
-				return ANSI_WHITE_BACKGROUND;
-			case "rb":
-				return ANSI_RED_BACKGROUND;
+			case "black":
+				return ANSI_BLACK;
             default:
                 return "";
         }
